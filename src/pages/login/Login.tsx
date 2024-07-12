af538cd5097e5a9c07e94f090342762628ba445d
Kelton <kelton.tinho23@gmail.com>
2024-07-12 14:37:09 -0300

    site funcional

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -10,85 +10,86 @@ import { RotatingLines } from 'react-loader-spinner';
 function Login() {
   let navigate = useNavigate();
 
-  const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
-    {} as UsuarioLogin
-  );
-
-  const { usuario, handleLogin } = useContext(AuthContext);
-
-  const {isLoading} = useContext(AuthContext) 
+  const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>({} as UsuarioLogin);
+  const { usuario, handleLogin, isLoading } = useContext(AuthContext);
 
   useEffect(() => {
     if (usuario.token !== "") {
-        navigate('/')
+      navigate('/');
     }
-}, [usuario])
+  }, [usuario, navigate]);
 
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-  setUsuarioLogin({
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuarioLogin({
       ...usuarioLogin,
       [e.target.name]: e.target.value
-  })
-}
+    });
+  }
 
-function login(e: ChangeEvent<HTMLFormElement>) {
-  e.preventDefault()
-console.log(usuarioLogin)
-  handleLogin(usuarioLogin)
-}
+  function login(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    handleLogin(usuarioLogin);
+  }
+
+  function irParaCadastro() {
+    navigate('/cadastro');
+  }
 
   return (
-    <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4" onSubmit={login}>
-          <h2 className="text-slate-900 text-5xl ">Entrar</h2>
-          <div className="flex flex-col w-full">
-            <label htmlFor="usuario">Usuário</label>
+    <div className="flex items-center justify-center w-full h-screen bg-gray-100">
+      <div className="max-w-md w-full p-8 bg-white shadow-lg rounded-lg">
+        <form className="flex flex-col gap-6" onSubmit={login}>
+          <h2 className="text-4xl text-gray-800 font-bold mb-4 text-center">Entrar</h2>
+          <div className="w-full">
+            <label htmlFor="usuario" className="block text-sm font-medium text-gray-700">Usuário</label>
             <input
               type="text"
               id="usuario"
               name="email"
-              placeholder="Usuario"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuarioLogin.email} 
+              placeholder="Usuário"
+              className="border-2 border-gray-300 rounded p-2 w-full"
+              value={usuarioLogin.email}
               onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="senha">Senha</label>
+          <div className="w-full">
+            <label htmlFor="senha" className="block text-sm font-medium text-gray-700">Senha</label>
             <input
               type="password"
               id="senha"
               name="senha"
               placeholder="Senha"
-              className="border-2 border-slate-700 rounded p-2"
-              value={usuarioLogin.senha} 
+              className="border-2 border-gray-300 rounded p-2 w-full"
+              value={usuarioLogin.senha}
               onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
             />
           </div>
-          <button  type='submit' className="rounded bg-indigo-400 hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
-           {isLoading ? <RotatingLines
-            strokeColor="white"
-            strokeWidth="5"
-            animationDuration="0.75"
-            width="24"
-            visible={true}
-          /> :
-            <span>Entrar</span>}
-          </button>
-
-          <hr className="border-slate-800 w-full" />
-
-          <p>
+          <div className="relative">
+            <button type='submit' className="rounded bg-indigo-500 hover:bg-indigo-700 text-white py-2 w-full">
+              <span className={isLoading ? 'opacity-0' : 'opacity-100'}>Entrar</span>
+              {isLoading && (
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <RotatingLines
+                    strokeColor="white"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    width="24"
+                    visible={true}
+                  />
+                </div>
+              )}
+            </button>
+          </div>
+          <hr className="border-gray-300 w-full my-6" />
+          <p className="text-center">
             Ainda não tem uma conta?{' '}
-            <Link to="/cadastro" className="text-indigo-800 hover:underline">
+            <Link to="/cadastro" className="text-indigo-800 hover:underline" onClick={irParaCadastro}>
               Cadastre-se
             </Link>
           </p>
         </form>
-        <div className="fundoLogin hidden lg:block"></div>
       </div>
-    </>
+    </div>
   );
 }
 
